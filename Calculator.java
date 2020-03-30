4edda54007c6e7bcc8868d2bb36cb848ac2c4eac
Christopher Lippek <christopher.lippek@fh-erfurt.de>
2020-03-30 10:37:29 +0200

    first

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+package de.fh_erfurt.omnichrom.schweizertaschenmesser;
+
+
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.widget.ImageView;
+
+/**
+ * not in use
+ */
+public class Calculator extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+    }
+
+}
